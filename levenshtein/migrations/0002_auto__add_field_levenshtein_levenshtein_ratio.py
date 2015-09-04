0b4122ab12e286bde104ac548e3496d818a0507d
nsiddarthreddy <nsiddarthreddy@gmail.com>
2015-09-04 11:51:53 -0700

    formatting changes to migration scripts

--- levenshtein/migrations/0002_auto__add_field_levenshtein_levenshtein_ratio.py
+++ levenshtein/migrations/0002_auto__add_field_levenshtein_levenshtein_ratio.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import SchemaMigration
-from django.db import models
 
 
 class Migration(SchemaMigration):
@@ -10,22 +8,25 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         # Adding field 'Levenshtein.levenshtein_ratio'
         db.add_column(u'levenshtein_levenshtein', 'levenshtein_ratio',
-                      self.gf('django.db.models.fields.FloatField')(null=True, blank=True),
+                      self.gf('django.db.models.fields.FloatField')
+                             (null=True, blank=True),
                       keep_default=False)
-
 
     def backwards(self, orm):
         # Deleting field 'Levenshtein.levenshtein_ratio'
         db.delete_column(u'levenshtein_levenshtein', 'levenshtein_ratio')
 
-
     models = {
         u'levenshtein.levenshtein': {
             'Meta': {'object_name': 'Levenshtein'},
-            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'levenshtein_ratio': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
-            'levenshtein_value': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [],
+                        {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [],
+                    {'primary_key': 'True'}),
+            'levenshtein_ratio': ('django.db.models.fields.FloatField', [],
+                                  {'null': 'True', 'blank': 'True'}),
+            'levenshtein_value': ('django.db.models.fields.IntegerField', [],
+                                  {'null': 'True', 'blank': 'True'}),
             'text_1': ('django.db.models.fields.TextField', [], {}),
             'text_2': ('django.db.models.fields.TextField', [], {})
         }
