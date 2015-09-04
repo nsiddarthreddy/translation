84220984b465c668a07f57c187d24575148c5dc0
nsiddarthreddy <nsiddarthreddy@gmail.com>
2015-09-04 11:51:53 -0700

    formatting changes to migration scripts

--- levenshtein/migrations/0003_initial.py
+++ levenshtein/migrations/0003_initial.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import SchemaMigration
-from django.db import models
 
 
 class Migration(SchemaMigration):
@@ -10,11 +8,15 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         # Adding model 'Levenshtein'
         db.create_table(u'levenshtein_levenshtein', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            (u'id', self.gf('django.db.models.fields.AutoField')
+                           (primary_key=True)),
             ('text_1', self.gf('django.db.models.fields.TextField')()),
             ('text_2', self.gf('django.db.models.fields.TextField')()),
-            ('levenshtein_value', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True)),
-            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('levenshtein_value',
+                self.gf('django.db.models.fields.IntegerField')
+                (null=True, blank=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')
+                               (auto_now_add=True, blank=True)),
         ))
         db.send_create_signal(u'levenshtein', ['Levenshtein'])
 
@@ -25,10 +27,14 @@ class Migration(SchemaMigration):
     models = {
         u'levenshtein.levenshtein': {
             'Meta': {'object_name': 'Levenshtein'},
-            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'levenshtein_ratio': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
-            'levenshtein_value': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField',
+                        [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [],
+                    {'primary_key': 'True'}),
+            'levenshtein_ratio': ('django.db.models.fields.FloatField', [],
+                                  {'null': 'True', 'blank': 'True'}),
+            'levenshtein_value': ('django.db.models.fields.IntegerField', [],
+                                  {'null': 'True', 'blank': 'True'}),
             'text_1': ('django.db.models.fields.TextField', [], {}),
             'text_2': ('django.db.models.fields.TextField', [], {})
         }
